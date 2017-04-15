d7fdbd3223c7e61398e4f9cf75cd6ed686c9fdba
Tim Miller <innerlogic4321@gmail.com>
2017-04-15 19:53:38 -0400

    Add more to the status view

---
+++ WinMasto/Tools/Converters/AddAtSignConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace WinMasto.Tools.Converters
+{
+    public class AddAtSignConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var accountNameValue = value as string;
+            return accountNameValue == null ? "@error" : $"@{accountNameValue}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WinMasto/Tools/Converters/CreatedTimeConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+using PrettyPrintNet;
+
+namespace WinMasto.Tools.Converters
+{
+    public class CreatedTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var accountDateTime = (DateTime) value;
+            var timespan = DateTime.Now.Subtract(accountDateTime);
+            return timespan.ToPrettyString(2, UnitStringRepresentation.Compact);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

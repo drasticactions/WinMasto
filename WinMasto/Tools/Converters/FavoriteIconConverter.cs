8f1e2566bfb15f674c84c8b7f5e8c54f0cecad60
Tim Miller <innerlogic4321@gmail.com>
2017-04-16 10:55:06 -0400

    Add FavoriteIconConverter.cs

---
+++ WinMasto/Tools/Converters/FavoriteIconConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+using FontAwesome.UWP;
+
+namespace WinMasto.Tools.Converters
+{
+    public class FavoriteIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            try
+            {
+                var favorited = (bool) value;
+                return favorited ? FontAwesomeIcon.Star : FontAwesomeIcon.StarOutline;
+            }
+            catch (Exception e)
+            {
+                return FontAwesomeIcon.StarOutline;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

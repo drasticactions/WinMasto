4b1d17595a8f719898322ba266cb6bcca7e882ab
Tim Miller <innerlogic4321@gmail.com>
2017-04-15 23:26:02 -0400

    Add count numbers for boosted / favorite

---
+++ WinMasto/Tools/Converters/CountVisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace WinMasto.Tools.Converters
+{
+    public class CountVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var number = (int) value;
+            return number > 0 ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

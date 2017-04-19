1b4db096d5801a45969bc8793dbaee5aaacb8bff
Tim Miller <innerlogic4321@gmail.com>
2017-04-19 16:20:45 -0400

    Add Reblog template

--- WinMasto/Tools/TemplateSelector/StatusTemplateSelector.cs
+++ WinMasto/Tools/TemplateSelector/StatusTemplateSelector.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Mastonet.Entities;
 
 namespace WinMasto.Tools.TemplateSelector
 {
@@ -12,9 +13,16 @@ namespace WinMasto.Tools.TemplateSelector
     {
         public DataTemplate RegularStatusDataTemplate { get; set; }
 
+        public DataTemplate ReblogStatusDataTemplate { get; set; }
+
         protected override DataTemplate SelectTemplateCore(object item,
             DependencyObject container)
         {
+            var status = item as Status;
+            if (status == null) return RegularStatusDataTemplate;
+
+            if (status.Reblog != null) return ReblogStatusDataTemplate;
+
             return RegularStatusDataTemplate;
         }
     }

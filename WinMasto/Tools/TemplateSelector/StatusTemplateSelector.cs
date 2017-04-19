65e2746a135ad5ce7b56fed3e04235360f96dcf4
Tim Miller <innerlogic4321@gmail.com>
2017-04-19 15:52:38 -0400

    Add StatusTemplateSelector

---
+++ WinMasto/Tools/TemplateSelector/StatusTemplateSelector.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace WinMasto.Tools.TemplateSelector
+{
+    public class StatusTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate RegularStatusDataTemplate { get; set; }
+
+        protected override DataTemplate SelectTemplateCore(object item,
+            DependencyObject container)
+        {
+            return RegularStatusDataTemplate;
+        }
+    }
+}

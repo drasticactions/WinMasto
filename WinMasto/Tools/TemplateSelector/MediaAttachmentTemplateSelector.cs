0423a07448cd65f64a72c8bf27bdb06ac0f94c51
Tim Miller <innerlogic4321@gmail.com>
2017-04-19 23:30:08 -0400

    Add template selector for media

---
+++ WinMasto/Tools/TemplateSelector/MediaAttachmentTemplateSelector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Mastonet.Entities;
+
+namespace WinMasto.Tools.TemplateSelector
+{
+    public class MediaAttachmentTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate ImageStatusDataTemplate { get; set; }
+
+        public DataTemplate VideoStatusDataTemplate { get; set; }
+
+        protected override DataTemplate SelectTemplateCore(object item,
+            DependencyObject container)
+        {
+            var status = item as Attachment;
+            return status.Url.Contains(".mp4") ? VideoStatusDataTemplate : ImageStatusDataTemplate;
+        }
+    }
+}

8c916655ea6f98720065d1d88186826abd365ef3
Tim Miller <innerlogic4321@gmail.com>
2017-05-03 15:19:20 -0400

    Add "reply to" text

---
+++ WinMasto/Tools/Converters/ReplyToStatusConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+using Mastonet.Entities;
+
+namespace WinMasto.Tools.Converters
+{
+    public class ReplyToStatusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var status = value as Status;
+            if (status == null) return "";
+            return $"↩️ Replying to {status.Account.AccountName}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
